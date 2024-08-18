1f42506f6794eb33fe43a9d674d96a30d97f9d1f
Douglas Toledo <dwtoledo@outlook.com>
2024-08-18 17:54:16 -0400

    Add Header Initial Content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,24 @@
+import { Amphora } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+
 export function Header() {
-  return <header>Header</header>
+  const navigate = useNavigate()
+
+  function handleLogoClick() {
+    navigate('/')
+  }
+
+  return (
+    <header>
+      <nav>
+        <span
+          className="flex items-center gap-1 hover:cursor-pointer"
+          onClick={handleLogoClick}
+        >
+          <Amphora />
+          Aromatherapy Shop
+        </span>
+      </nav>
+    </header>
+  )
 }
