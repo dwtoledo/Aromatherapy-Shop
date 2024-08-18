fcc0d7ca9caa55db459821d9e318e53393d0804c
Douglas Toledo <dwtoledo@outlook.com>
2024-08-18 18:14:47 -0400

    Implement Initial Login Content

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,10 +1,40 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const loginForm = z.object({
+  email: z.string().email(),
+})
+
+type LoginForm = z.infer<typeof loginForm>
 
 export function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<LoginForm>()
+
+  async function handleLogin(data: LoginForm) {
+    console.log(data)
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    // TODO - Implement Login HTTP Request
+  }
+
   return (
     <main className="flex-1">
       <Helmet title="Login" />
-      <h1>Login</h1>
+      <h1>Access panel login</h1>
+      <p>Track your sales through the partner dashboard!</p>
+      <form onSubmit={handleSubmit(handleLogin)}>
+        <Label htmlFor="email">Your e-mail:</Label>
+        <Input id="email" type="email" {...register('email')} />
+        <Button disabled={isSubmitting}>Login</Button>
+      </form>
     </main>
   )
 }
