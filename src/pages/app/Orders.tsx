684d2820d73a02cb29987a3fc8ec845ab85f75d3
Douglas Toledo <dwtoledo@outlook.com>
2024-11-28 13:02:11 -0500

    Replace Mocked Orders from const to JSON Server
    Implement Paginator component

--- src/pages/app/Orders.tsx
+++ src/pages/app/Orders.tsx
@@ -1,12 +1,17 @@
+/* eslint-disable @stylistic/max-len */
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Filter, FilterX } from 'lucide-react'
+import { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 import { OrdersTable } from '@/components/OrdersTable'
+import { Paginator } from '@/components/Paginator'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Order } from '@/models/Order'
+import { PageableResult } from '@/models/Pageable'
 
 const filtersForm = z.object({
   customerName: z
@@ -31,13 +36,16 @@ type FiltersForm = z.infer<typeof filtersForm>
 function ErrorMessage({ message }: { message?: string }) {
   if (!message) return null
   return (
-    <p className="text-destructive text-xs leading-relaxed italic">
-      {message}
-    </p>
+    <p className="text-xs italic leading-relaxed text-destructive">{message}</p>
   )
 }
 
 export function Orders() {
+  const [orders, setOrders] = useState<PageableResult<Order>>(
+    {} as PageableResult<Order>,
+  )
+  const [loading, setLoading] = useState(true)
+
   const {
     register,
     handleSubmit,
@@ -47,13 +55,63 @@ export function Orders() {
     resolver: zodResolver(filtersForm),
   })
 
+  async function fetchOrders(pageIndex = 1, perPage = 10) {
+    setLoading(true)
+    try {
+      const params = new URLSearchParams({
+        _page: pageIndex.toString(),
+        _per_page: perPage.toString(),
+      })
+      const url = `${import.meta.env.VITE_BACKEND_URL}/orders?${params.toString()}`
+      const response = await fetch(url)
+
+      if (!response.ok) {
+        // TODO - implement error handle
+        console.log(`Error: ${response.status} - ${response.statusText}`)
+      }
+      const orders: PageableResult<Order> = await response.json()
+      return orders
+    } catch (error) {
+      // TODO - implement error handle
+      console.error('Failed to fetch orders:', error)
+      return {} as PageableResult<Order>
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    const getOrders = async () => {
+      try {
+        const orders = await fetchOrders(1)
+        setOrders(orders)
+      } catch (error) {
+        // TODO - implement error handle
+        console.log(error)
+      }
+    }
+    getOrders()
+  }, [])
+
   function handleFiltersSubmit(data: FiltersForm) {
     // TODO - implement filters
+    console.log(data)
   }
 
   function handleClearFiltersClick() {
-    reset()
     // TODO - implement clear filters
+    reset()
+  }
+
+  async function handlePageChange(page: number | null, perPage: number) {
+    if (page === null) return
+    const orders = await fetchOrders(page, perPage)
+    setOrders(orders)
+  }
+
+  async function handlePerPageChange(perPage: number) {
+    const orders = await fetchOrders(1, perPage)
+    setOrders(orders)
   }
 
   return (
@@ -64,12 +122,7 @@ export function Orders() {
         <span>Filters:</span>
 
         <form onSubmit={handleSubmit(handleFiltersSubmit)}>
-
-          <Input
-            placeholder="Order ID"
-            type="text"
-            {...register('orderId')}
-          />
+          <Input placeholder="Order ID" type="text" {...register('orderId')} />
           <ErrorMessage message={errors.orderId?.message} />
 
           <Input
@@ -90,10 +143,19 @@ export function Orders() {
           </Button>
         </form>
 
-        <OrdersTable />
+        {orders?.data?.length && !loading
+          ? <OrdersTable orders={orders.data} />
+          : <span>Loading orders.</span>}
 
+        <Paginator
+          onPageChange={handlePageChange}
+          onPerPageChange={handlePerPageChange}
+          last={orders.last}
+          next={orders.next}
+          pages={orders.pages}
+          prev={orders.prev}
+        />
       </div>
-
     </main>
   )
 }
